a9fabcafd75c450ad274456969d7c3938cfb6296
ParkYongHo <qkaxhf8823@naver.com>
2026-05-03 16:27:49 +0900

    fix: Gmail SMTP secure 옵션 추가 및 포트 465 변경

--- src/app.module.ts
+++ src/app.module.ts
@@ -42,6 +42,7 @@ import { MailerModule } from '@nestjs-modules/mailer';
       transport: {
         host: process.env.MAIL_HOST,
         port: Number(process.env.MAIL_PORT),
+        secure: Number(process.env.MAIL_PORT) === 465,
         auth: {
           user: process.env.MAIL_USER,
           pass: process.env.MAIL_PASS,
